94a93c2c0932799e2dd2ad65665cdc850fc9c19a
Maina Evans <kinuthiafabiankinuthia@gmail.com>
2023-02-11 00:31:57 +0300

    Library and Servics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { PostsComponent } from './posts/posts.component';
 import { PostItemComponent } from './post-item/post-item.component';
 import { HistoryComponent } from './history/history.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
@@ -25,6 +26,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     BrowserModule,
     AppRoutingModule,
     BooksModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
